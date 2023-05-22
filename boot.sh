56eb063d90448c747472d639becfa0c94367c233
chen <41134701+Alpha-CL@users.noreply.github.com>
2023-05-22 17:19:43 +0800

    Update boot.sh
    fix(main): kill prev process program

--- boot.sh
+++ boot.sh
@@ -10,4 +10,5 @@ while true; do
     sleep 5
 done
 # run start app
+kill -9 $(ps aux | grep 'runserver' | awk '{print $2}')
 exec gunicorn --workers 4 -b :18080 --worker-class gthread --access-logfile - --error-logfile - runserver:app
