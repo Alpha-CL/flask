01326c34c1d4e49e7e8cd21f92f27b95c5b179c1
chen <41134701+Alpha-CL@users.noreply.github.com>
2023-05-26 16:59:10 +0800

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source ./venv/bin/activate
+source venv/bin/activate
 # init database
 while true; do
     flask db upgrade
@@ -10,5 +10,5 @@ while true; do
     sleep 5
 done
 # run start app
-kill -9 $(ps aux | grep 'runserver' | awk '{print $2}')
+pkill -f boot.sh && kill -9 $(ps aux | grep 'runserver' | awk '{print $2}')
 exec gunicorn --workers 4 -b :18080 --worker-class gthread --access-logfile - --error-logfile - runserver:app
